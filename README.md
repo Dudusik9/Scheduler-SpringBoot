e6887c614aa0267487b73907f512756cf811c011
dmitry <dmitry@example.ru>
2020-09-16 09:23:49 +0300

    Third commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Scheduler-SpringBoot
+Планировщик реализованный при помощи Spring Boot;
+Используемая БД - H2;
+Адрес для доступа localhost:8888;
--- src(08.09.20)/main/java/scheduler/repository/ScriptDAO.java
+++
@@ -1,9 +0,0 @@
-package scheduler.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import scheduler.model.Scripts;
-
-@Repository
-public interface ScriptDAO extends CrudRepository<Scripts, Long> {
-}
